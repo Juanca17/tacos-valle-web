357375fd8b1f1b781329e905bf1f3274ce1dadaf
Juanca17 <juancasan17@gmail.com>
2025-03-12 00:11:25 -0600

    Test favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,30 @@
 import "./globals.css";
+const basePath =
+  process.env.NODE_ENV === "production" ? "/tacos-valle-web" : "";
 
 export const metadata = {
   title: "Tacos del Valle",
   description: "Una taquería de la vieja escuela con un toque moderno.",
   icons: {
-    icon: "/favicon/favicon.ico",
-    shortcut: "/favicon/favicon.ico",
-    apple: "/favicon/apple-touch-icon.png",
+    icon: `${basePath}/favicon/favicon.ico`,
+    shortcut: `${basePath}/favicon/favicon.ico`,
+    apple: `${basePath}/favicon/apple-touch-icon.png`,
     other: [
       {
         rel: "icon",
-        url: "/favicon/android-chrome-192x192.png",
+        url: `${basePath}/favicon/android-chrome-192x192.png`,
         sizes: "192x192",
         type: "image/png",
       },
       {
         rel: "icon",
-        url: "/favicon/android-chrome-512x512.png",
+        url: `${basePath}/favicon/android-chrome-512x512.png`,
         sizes: "512x512",
         type: "image/png",
       },
     ],
   },
-  manifest: "/favicon/site.webmanifest",
+  manifest: `${basePath}/favicon/site.webmanifest`,
   openGraph: {
     url: "https://tacosvalle.com",
     title: "Tacos Valle - Auténtico Sabor Mexicano",
