12b34a82c046eacd1839ef6475a0e86d62ffbd2e
Juanca17 <juancasan17@gmail.com>
2025-04-20 18:30:03 -0600

    Update orange marquee

--- src/app/sections/MarqueeOrange.jsx
+++ src/app/sections/MarqueeOrange.jsx
@@ -1,4 +1,8 @@
 export const MarqueeOrange = () => {
+  const palabras = ["TACOS", "BOTANAS", "CERVEZAS"];
+  // Creamos un array de 10 repeticiones de ["TACOS","BOTANAS","CERVEZAS"] y lo aplanamos
+  const repeatedWords = Array(10).fill(palabras).flat();
+
   return (
     <section
       className="w-full h-32 overflow-hidden flex items-center relative"
@@ -7,16 +11,14 @@ export const MarqueeOrange = () => {
       <div className="flex whitespace-nowrap animate-marquee">
         {[...Array(2)].map((_, i) => (
           <div key={i} className="flex">
-            {Array(10)
-              .fill("TACOS, BOTANAS Y CERVEZAS")
-              .map((text, index) => (
-                <span
-                  key={`${i}-${index}`}
-                  className="font-jean-luc-thin text-[2.5rem] sm:text-[4rem] mx-8 inline-block"
-                >
-                  {text}
-                </span>
-              ))}
+            {repeatedWords.map((text, index) => (
+              <span
+                key={`${i}-${index}`}
+                className="font-jean-luc-thin text-[2.5rem] sm:text-[4rem] mx-8 inline-block"
+              >
+                {text}
+              </span>
+            ))}
           </div>
         ))}
       </div>
